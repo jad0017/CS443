32fe0d4d4d1048691ba5ebbcc30678105c3c2918
Christy Lafayette <evybay@gmail.com>
2016-03-24 22:40:53 -0500

    4:1:1 subsampling and some minor fixes for color.py

--- compressor/Chroma.py
+++ compressor/Chroma.py
@@ -25,14 +25,71 @@ def sampling(A, X, Y):
     elif (X == 3) or (Y == 3):
         print("3 is not a valid number for chroma subsampling. Please enter different numbers.")
     else:
-        print("Hooray you can sample!") #debug obv
-        #actual sampling goes here
+        if (A==4) and (X==4) and (Y==4):
+            print("4:4:4 subsampling") #trivial -- no change, but it's here for completeness
 
-sample_in = input('Please enter chroma subsampling values as A:B:C - ')
+        elif (A==4) and (X==2) and (Y==0):
+            print("4:2:0 subsampling")
+            yin_420= input('Please enter your file name for the Y channel image, including the extension: ')
+            cbin_420= input('Please enter your file name for the Cb channel image, including the extension: ')
+            crin_420= input('Please enter your file name for the Cr channel image, including the extension: ')
+            #sample_420(yin_420,cbin_420,crin_420)
+
+        elif (A==4) and (X==1) and (Y==1):
+            print("4:1:1 subsampling")
+            yin_411= input('Please enter your file name for the Y channel image, including the extension: ')
+            cbin_411= input('Please enter your file name for the Cb channel image, including the extension: ')
+            crin_411= input('Please enter your file name for the Cr channel image, including the extension: ')
+            sample_411(cbin_411)
+            sample_411(crin_411)
+
+        else:
+            print("Something went wrong.")
+
+def sample_411(imgstring):
+    """
+    Perform 4:1:1 subsampling on an image.
+    The actual result is barely noticeable (I couldn't actually tell it had changed anything), but examining it in photoshop, it does perform as expected, thankfully.
+
+    :param imgstring: a string of the target image's name
+
+    :returns: an image with the filenamesubsampled_411_imgstring
+    """
+
+    try:
+        im = Image.open(imgstring)
+    except IOError:
+        print('This was an invalid file. Please try again.')
+        sys.exit(0)
+    # DEBUG: just making sure it's reading images correctly, remove
+    #im.show()
+
+    for Y in range (im.size[1]):
+        for X in range (im.size[0]):
+            i = 1
+            if (i == 1):
+                (r,g,b) = im.getpixel((X,Y))
+                i = i + 1
+            elif (1 < i < 4):
+                im.putpixel((X,Y), (r,g,b))
+                i = i + 1
+            else:
+                im.putpixel((X,Y), (r,g,b))
+                i = 1
+            #if imageIndex == 0:
+            #    y = min(int(.299*r + .587*g + .144*b), 255)
+            #    r = g = b = y
+
+            #im.putpixel((X,Y), (r,g,b))
+
+    outname = "subsampled_411_" + imgstring
+    im.save(outname)
+
+sample_in = input('Please enter chroma subsampling values in A:B:C form \nAvaliable values - 4:4:4, 4:2:0, 4:1:1 \nChroma Subsampling - ')
 try:
     A1, X1, Y1 =sample_in.split(':', 2)
 except ValueError:
     print('This input is invalid.')
     sys.exit(0)
 sampling(A1, X1, Y1)
-print(A1, X1, Y1)
+print(A1, X1, Y1) #DEBUG - remove
--- compressor/Colors.py
+++ compressor/Colors.py
@@ -1,35 +1,36 @@
 import sys
-import shutil
-import os
-import struct
 
 from PIL import Image
 
 def usage():
-	print("Usage: %s filename.jpg" % (sys.argv[0]))
-	exit()
+    print("Usage: %s filename.jpg" % (sys.argv[0]))
+    exit()
 
 def rgb_to_ycbcr(fname, outname, imageIndex):
-	im = Image.open(fname) #Can be many different formats.
-	for Y in range (im.size[1]):
-		for X in range (im.size[0]):
-			(r,g,b) = im.getpixel((X,Y))
+    im = Image.open(fname) #Can be many different formats.
+    for Y in range (im.size[1]):
+        for X in range (im.size[0]):
+            (r,g,b) = im.getpixel((X,Y))
 
-			if imageIndex == 0:
-				y = min(int(.299*r + .587*g + .144*b), 255)
-				r = g = b = y
-			elif imageIndex == 1:
-				cb = int((-.168736*r) + (-.331264*g) + (b/2.0) + 128)
-				r = g = 255 - cb
-				b = cb
-			else:
-				cr = int((.5*r) + (-.418688*g) + (-.081312*b) + 128)
-				b = g = 255 - cr
-				r = cr
+            if imageIndex == 0:
+                y = min(int(.299*r + .587*g + .144*b), 255)
+                r = g = b = y
+            elif imageIndex == 1:
+                cb = int((-.168736*r) + (-.331264*g) + (b/2.0) + 128)
+                r = g = 255 - cb
+                b = cb
+            else:
+                cr = int((.5*r) + (-.418688*g) + (-.081312*b) + 128)
+                b = g = 255 - cr
+                r = cr
 
-			im.putpixel((X,Y), (r,g,b))
+            im.putpixel((X,Y), (r,g,b))
 
-	im.save(outname)
+    im.save(outname)
+
+def ycbcr_to_rgb(fnamey, fnamecb, fnamecr, outname, imageIndex):
+    im = Image.open(fname) #Can be many different formats.
+    # etc
 
 fname = sys.argv[1]
 oname = fname.split(".")
@@ -37,6 +38,7 @@ y = oname[0] + "_y." + oname[-1]
 cb = oname[0] + "_cb." + oname[-1]
 cr = oname[0] + "_cr." + oname[-1]
 
-rgb_to_ycbcr(fname,y, 0)
-rgb_to_ycbcr(fname,cb, 1)
-rgb_to_ycbcr(fname,cr, 2)
+# Call examples --
+# rgb_to_ycbcr(fname,y, 0)
+# rgb_to_ycbcr(fname,cb, 1)
+# rgb_to_ycbcr(fname,cr, 2)

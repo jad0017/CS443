1be059da9d7ee284df8ea53e64b3eb9e6de7b587
jad0017 <jad0017@uah.edu>
2016-03-27 15:04:52 -0500

    Add single-mode to Colors.py + Add all error calculations

--- Compressor/Colors.py
+++ Compressor/Colors.py
@@ -1,20 +1,32 @@
 from . import Matrix
 
+def YCbCr_to_RGB_single(y, cb, cr):
+    crt = cr - 128
+    cbt = cb - 128
+
+    r = int(y + (1.402 * crt))
+    g = int(y - (0.34414 * cbt) - (0.71414 * crt))
+    b = int(y + (1.772 * cbt))
+
+    return (r, g, b)
+
+
 def YCbCr_to_RGB(M):
     A = Matrix.zero_matrix(len(M[0]), len(M))
 
     for y in range(len(M)):
         for x in range(len(M[0])):
             (y, cb, cr) = M[y][x]
-            cr -= 128
-            cb -= 128
-
-            r = int(y + (1.402 * cr))
-            g = int(y - (0.34414 * cb) - (0.71414 * cr))
-            b = int(y + (1.772 * cb))
-
-            A[y][x] = (r, g, b)
+            A[y][x] = YCbCr_toRGB_single(y, cb, cr)
     return A
+
+
+def RGB_to_YCbCr_single(r, g, b):
+    y  = int((0.299 * r) + (0.587 * g) + (0.144 * b))
+    cb = int((-0.168736 * r) + (-0.331264 * g) + (0.5 * b) + 128)
+    cr = int((0.5 * r) + (-0.418688 * g) + (-0.081312 * b) + 128)
+
+    return (y, cb, cr)
 
 
 def RGB_to_YCbCr(M):
@@ -23,12 +35,7 @@ def RGB_to_YCbCr(M):
     for y in range(len(M)):
         for x in range(len(M[0])):
             (r, g, b) = M[y][x]
-
-            y  = int((0.299 * r) + (0.587 * g) + (0.144 * b))
-            cb = int((-0.168736 * r) + (-0.331264 * g) + (0.5 * b) + 128)
-            cr = int((0.5 * r) + (-0.418688 * g) + (-0.081312 * b) + 128)
-
-            A[y][x] = (y, cb, cr)
+            A[y][x] = RGB_to_YCbCr_single(r, g, b)
     return A
 
 

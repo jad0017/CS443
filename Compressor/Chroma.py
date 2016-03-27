9afbe1437dff05bfc7238253a7cb8f4e8afd3906
jad0017 <jad0017@uah.edu>
2016-03-27 14:40:34 -0500

    Update Chroma.py + light cleanup of pcrn.py

--- Compressor/Chroma.py
+++ Compressor/Chroma.py
@@ -1,109 +1,79 @@
 import sys
 
 from PIL import Image
-import Compressor.Matrix
+from . import Matrix
 
-def sampling(A, X, Y):
-    """
-    Define the chroma subsampling to be used... Error checking hell
-    """
+def validate_spec(spec):
+    A = spec.split(':')
+    if len(A) != 3:
+        print("Invalid Chroma Subsampling format.")
+        sys.exit(1)
+
     try:
-        A = int(A)
-        X = int(X)
-        Y = int(Y)
+        a = int(A[0])
+        b = int(A[1])
+        c = int(A[2])
     except ValueError:
-        print('This input is invalid.')
-        sys.exit(0)
+        print("Invalid Chroma Subsampling format.")
+        sys.exit(1)
 
-    if (A < 0) or (X < 0) or (Y < 0):
-        print("This value is invalid. Chroma subsampling must use positive integers.")
-    elif (A > 4) or (X > 4) or (Y > 4):
-        print("This value is invalid. Chroma subsampling must not be greater than 4.")
-    elif (A != 4):
-        print("This value is invalid. Chroma subsampling must use 4 for its first value.")
-    elif (X == 0) and (Y == 0):
-        print("4:0:0 subsampling is not possible.. Please enter different numbers.")
-    elif (X == 3) or (Y == 3):
-        print("3 is not a valid number for chroma subsampling. Please enter different numbers.")
-    else:
-        if (A==4) and (X==4) and (Y==4):
-            print("4:4:4 subsampling") #trivial -- no change, but it's here for completeness
+    if A != 4:
+        print("Chroma Subsampling must use 4 for the first value.")
+        sys.exit(1)
 
-        elif (A==4) and (X==2) and (Y==0):
-            print("4:2:0 subsampling")
-            yin_420= input('Please enter your file name for the Y channel image, including the extension: ')
-            cbin_420= input('Please enter your file name for the Cb channel image, including the extension: ')
-            crin_420= input('Please enter your file name for the Cr channel image, including the extension: ')
-            sample_420(cbin_420)
-            sample_420(crin_420)
+    if (not b in [0, 1, 2, 4]) or (not c in [0, 1, 2, 4]):
+        print("Chroma Subsampling value must be in the set {0, 1, 2, 4}.")
+        sys.exit(1)
 
-        elif (A==4) and (X==1) and (Y==1):
-            print("4:1:1 subsampling")
-            yin_411= input('Please enter your file name for the Y channel image, including the extension: ')
-            cbin_411= input('Please enter your file name for the Cb channel image, including the extension: ')
-            crin_411= input('Please enter your file name for the Cr channel image, including the extension: ')
-            sample_411(cbin_411)
-            sample_411(crin_411)
+    if (b == 0) and (c == 0):
+        print("4:0:0 Chroma Subsampling is not possible.")
+        sys.exit(1)
 
+    # Not allowing these for now... not worth the effort.
+    if (b == 0):
+        print("4:0:x Chroma Subsampling is not possible.")
+        sys.exit(1)
+
+    return (b, c)
+
+
+def chroma_subsample(M, spec):
+    """
+    Perform subsampling on a matrix.
+
+    :param M: Matrix to perform sampling on.
+    :param spec: Subsampling spec
+
+    :returns: SampledMatrix: a matrix with the subsampled data
+    """
+    S = validate_spec(spec)
+
+    if (S[0] == 4) and (S[1] == 4):
+        return M
+
+    A = Matrix.zero_matrix(len(M[0]), len(M))
+
+    mod_lookup = { 4:1, 2:2, 1:4, 0:0 }
+    x1mod = mod_lookup[S[0]]
+    x2mod = mod_lookup[S[1]]
+
+    V = None
+    for y in range(len(M)):
+        if (y % 2 == 0):
+            # First row (no checking for 0 at the moment).
+            for x in range(len(M[0])):
+                if (x % x1mod == 0):
+                    V = M[y][x]
+                A[y][x] = V
         else:
-            print("Something went wrong.")
+            # Second row
+            for x in range(len(M[0])):
+                if (S[1] == 0):
+                    A[y][x] = A[y - 1][x]
+                elif (x % x2mod == 0):
+                    V = M[y][x]
+                A[y][x] = v
+    return A
 
-def sample_420(M):
-    """
-    Perform 4:2:0 subsampling on an image.
 
-    :param M: Matrix to perform sampling on.
-
-    :returns: SampledMatrix: a matrix with the subsampled data
-    """
-    SampledMatrix = Compressor.Matrix.zero_matrix(len(M[0]), len(M))
-
-    for Y in range (len(M)):
-        if (Y % 2) == 0: #if it's the first row, then copy every other pixel
-            for X in range (len(M[0])):
-                i = 1
-                if (i == 1):
-                    (r,g,b) = M[Y][X]
-                    SampledMatrix[Y][X] = (r,g,b)
-                    i = i + 1
-                else:
-                    SampledMatrix[Y][X] = (r,g,b)
-                    i = 1
-
-        else: #if it's an even row, then copy pixel from the row above it
-            for X in range (len(M[0])):
-                (r,g,b) = M[Y-1][X]
-                SampledMatrix[Y][X] = (r,g,b)
-
-    return SampledMatrix
-
-def sample_411(M):
-    """
-    Perform 4:1:1 subsampling on an image.
-    The actual result is barely noticeable (I couldn't actually tell it had changed anything), but examining it in photoshop, it does perform as expected, thankfully.
-
-    :param M: Matrix to perform sampling on.
-
-    :returns: SampledMatrix: a matrix with the subsampled data
-    """
-
-    SampledMatrix = Compressor.Matrix.zero_matrix(len(M[0]), len(M))
-
-    for Y in range (len(M)):
-        for X in range (len(M[0])):
-            if (X % 4) == 0:
-                (r,g,b) = M[Y][X]
-                SampledMatrix[Y][X] = (r,g,b)
-            else:
-                SampledMatrix[Y][X] = (r,g,b)
-
-    return SampledMatrix
-
-sample_in = input('Please enter chroma subsampling values in A:B:C form \nAvaliable values - 4:4:4, 4:2:0, 4:1:1 \nChroma Subsampling - ')
-try:
-    A1, X1, Y1 =sample_in.split(':', 2)
-except ValueError:
-    print('This input is invalid.')
-    sys.exit(0)
-sampling(A1, X1, Y1)
-print(A1, X1, Y1) #DEBUG - remove
+# vim:ts=4:sws=4:st=4:ai
--- pcrn.py
+++ pcrn.py
@@ -1,15 +1,15 @@
 #The PIL python library is required to run this script.
 
 import sys
-import shutil
+#import shutil
 import os
 import struct
 
 from PIL import Image
 
 def usage():
-	print "Usage: %s file1.jpg file2.jpg" % (sys.argv[0])
-	exit()
+	print("Usage: %s file1.jpg file2.jpg" % (sys.argv[0],))
+	sys.exit(1)
 
 def mse(img1, img2):
 	im1 = Image.open(img1)
@@ -19,21 +19,21 @@ def mse(img1, img2):
 	bsum = 0
 
 	if not im1.size == im2.size:
-		print "Images are different sizes"
-		exit(1)
+		print("Images are different sizes!")
+		sys.exit(1)
 
-	for Y in range (im1.size[1]):
-		for X in range (im1.size[0]):
-			(r1,g1,b1) = im1.getpixel((X,Y))
-			(r2,g2,b2) = im2.getpixel((X,Y))
-			rsum += pow(r1-r2,2)
-			gsum += pow(g1-g2,2)
-			bsum += pow(b1-b2,2)
+	for Y in range(im1.size[1]):
+		for X in range(im1.size[0]):
+			(r1, g1, b1) = im1.getpixel((X,Y))
+			(r2, g2, b2) = im2.getpixel((X,Y))
+			rsum += pow(r1 - r2, 2)
+			gsum += pow(g1 - g2, 2)
+			bsum += pow(b1 - b2, 2)
 
 	scalar = (im1.size[0] * im2.size[1])
 
 	return (rsum/scalar,gsum/scalar,bsum/scalar)
-	
+
 
 if not len(sys.argv) is 3:
 	usage()
